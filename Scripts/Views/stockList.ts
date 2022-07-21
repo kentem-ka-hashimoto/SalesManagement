3894715c798da4f2cd3c10229ce0ffce1c8b1d50
ka-hashimoto <ka-hashimoto@kentem.co.jp>
2022-07-21 11:53:01 +0900

    仕入処理画面でローカルストレージで保存をした際にリアルタイムで在庫一覧を更新する処理を実装

--- Scripts/Views/stockList.ts
+++ Scripts/Views/stockList.ts
@@ -1,5 +1,57 @@
+import { Product } from '../Models/product.js';
+import { Global } from '../Models/global.js';
+
+// tbodyの取得
+const tbody: HTMLTableSectionElement | null = document.querySelector('tbody');
+// 閉じるボタンの取得
 const closeBtn = document.getElementById('close') as HTMLButtonElement;
 
+window.onload = function () {
+  deleteTbodyChildren();
+  createStockList();
+};
+
+window.addEventListener('storage', (event) => {
+  if (event.storageArea != localStorage) return;
+  if (event.key === 'stock') {
+    location.reload();
+  }
+});
+
+// 在庫一覧の作成
+function createStockList(): void {
+  Global.getStockFromLocalStorage();
+
+  Global.stockManager.stockArr.forEach((target: Product) => {
+    const tr: HTMLTableRowElement = document.createElement('tr');
+    const tdName: HTMLTableCellElement = document.createElement('td');
+    tdName.textContent = target.productName;
+    const tdPurchaseDate: HTMLTableCellElement = document.createElement('td');
+    tdPurchaseDate.textContent = target.purchaseDate;
+    const tdPurchasePrice: HTMLTableCellElement = document.createElement('td');
+    tdPurchasePrice.textContent = `${target.purchasePrice}円`;
+    const tdSellingPrice: HTMLTableCellElement = document.createElement('td');
+    tdSellingPrice.textContent = `${target.sellingPrice}円`;
+    const tdStock: HTMLTableCellElement = document.createElement('td');
+    tdStock.textContent = `${target.stock}個`;
+
+    tr.appendChild(tdName);
+    tr.appendChild(tdPurchaseDate);
+    tr.appendChild(tdPurchasePrice);
+    tr.appendChild(tdSellingPrice);
+    tr.appendChild(tdStock);
+
+    tbody?.appendChild(tr);
+  });
+}
+
+// tbody内の削除
+function deleteTbodyChildren(): void {
+  while (tbody?.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+}
+// 閉じるボタンの処理
 closeBtn.addEventListener('click', () => {
   window.close();
 });
