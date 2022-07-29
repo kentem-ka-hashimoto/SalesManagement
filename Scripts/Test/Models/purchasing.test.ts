9861373349a70e120d26e62f34bbd89171cb4741
ka-hashimoto <ka-hashimoto@kentem.co.jp>
2022-07-29 15:10:01 +0900

    テストコード内にてコンストラクターの引数にDate型ではなくstringを渡していたため修正

--- Scripts/Test/Models/purchasing.test.ts
+++ Scripts/Test/Models/purchasing.test.ts
@@ -3,11 +3,12 @@ import { Purchasing } from '../../Models/purchasing.js';
 
 describe('PurchasingTest', () => {
   test('propertyTest', () => {
-    let purchasing: Purchasing = new Purchasing(new Product('桃'), '2022-07-20', 200, 400, 200);
+    const date: Date = new Date('2022-07-20');
+    let purchasing: Purchasing = new Purchasing(new Product('桃'), new Date('2022-07-20'), 200, 400, 200);
     // 商品名の取得
     expect(purchasing.product.name).toBe('桃');
     // 仕入日の取得
-    expect(purchasing.purchaseDate).toBe('2022-07-20');
+    expect(purchasing.purchaseDate).toEqual(date);
     // 仕入価格の取得
     expect(purchasing.purchasePrice).toBe(200);
     // 販売価格の取得
--- Scripts/Test/Models/stockManager.test.ts
+++ Scripts/Test/Models/stockManager.test.ts
@@ -2,7 +2,6 @@ import { Product } from '../../Models/product.js';
 import { StockManager } from '../../Models/stockManager.js';
 import { Purchasing } from '../../Models/purchasing.js';
 
-
 describe('StockManagerTest', () => {
   test('propertyTest', () => {
     let manager = new StockManager();
@@ -12,8 +11,8 @@ describe('StockManagerTest', () => {
 
   test('addTest', () => {
     let manager = new StockManager();
-    let purchasing:Purchasing=new Purchasing(new Product('桃'), '2022-07-20', 200, 400, 200)
-
+    let date: Date = new Date('2022-07-20');
+    let purchasing: Purchasing = new Purchasing(new Product('桃'), new Date('2022-07-20'), 200, 400, 200);
 
     // 最初何もないことを確認
     expect(manager.stockArr.length).toBe(0);
@@ -22,19 +21,20 @@ describe('StockManagerTest', () => {
     manager.add(purchasing);
     expect(manager.stockArr.length).toBe(1);
     expect(manager.stockArr[0].product.name).toBe('桃');
-    expect(manager.stockArr[0].purchaseDate).toBe('2022-07-20');
+    expect(manager.stockArr[0].purchaseDate).toEqual(date);
     expect(manager.stockArr[0].purchasePrice).toBe(200);
     expect(manager.stockArr[0].sellingPrice).toBe(400);
     expect(manager.stockArr[0].stock).toBe(200);
 
     // 2つ目追加した際の確認
-    purchasing = new Purchasing(new Product('お茶'), '2022-07-20', 60, 150, 600);
+    date = new Date('2022-07-20');
+    purchasing = new Purchasing(new Product('お茶'), new Date('2022-07-20'), 60, 150, 600);
     manager.add(purchasing);
     expect(manager.stockArr.length).toBe(2);
     expect(manager.stockArr[1].product.name).toBe('お茶');
     expect(manager.stockArr[1].stock).toBe(600);
     expect(manager.stockArr[1].purchasePrice).toBe(60);
     expect(manager.stockArr[1].sellingPrice).toBe(150);
-    expect(manager.stockArr[1].purchaseDate).toBe('2022-07-20');
+    expect(manager.stockArr[1].purchaseDate).toEqual(date);
   });
 });
