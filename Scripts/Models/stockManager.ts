13166af3d1bad94325e32a072f6c6763036cb8b7
ka-hashimoto <ka-hashimoto@kentem.co.jp>
2022-08-04 10:56:29 +0900

    仕入日が古いものから在庫を減らす処理を実装、テストの追加及び成功確認

--- Scripts/Models/stockManager.ts
+++ Scripts/Models/stockManager.ts
@@ -3,6 +3,7 @@ import { Product } from './product.js';
 import { SalesManager } from './salesManager.js';
 
 export class StockManager {
+  private readonly ABNORMAL_VALUE_ERROR: string = 'The value is abnormal';
   private _stockArr: Purchasing[] = [];
 
   public get stockArr(): Purchasing[] {
@@ -13,6 +14,32 @@ export class StockManager {
     this._stockArr.push(purchase);
     this.sortAscendingOrder();
     this._stockArr = this.separateByProduct(this._stockArr, products);
+  }
+
+  public reduceStock(saleProductName: string, saleQuantity: number): void {
+    // 在庫があるかチェック
+    let stock: number = 0;
+    this._stockArr.forEach((item: Purchasing) => {
+      if (item.product.name === saleProductName) {
+        stock += item.stock;
+      }
+    });
+    if (stock < saleQuantity) {
+      throw new RangeError(this.ABNORMAL_VALUE_ERROR);
+    }
+
+    // 在庫を減らす
+    for (let i = 0; i < this._stockArr.length; i++) {
+      if (this._stockArr[i].product.name === saleProductName) {
+        if (this._stockArr[i].stock >= saleQuantity) {
+          this._stockArr[i].stock -= saleQuantity;
+          break;
+        } else if (this._stockArr[i].stock < saleQuantity) {
+          saleQuantity -= this._stockArr[i].stock;
+          this._stockArr[i].stock = 0;
+        }
+      }
+    }
   }
 
   private separateByProduct(purchases: Purchasing[], products: Product[]): Purchasing[] {
--- Scripts/Test/Models/stockManager.test.ts
+++ Scripts/Test/Models/stockManager.test.ts
@@ -13,12 +13,13 @@ describe('StockManagerTest', () => {
     let manager = new StockManager();
     let date: Date = new Date('2022-07-20');
     let purchasing: Purchasing = new Purchasing(new Product('桃'), new Date('2022-07-20'), 200, 400, 200);
+    const productArr: Product[] = [new Product('桃')];
 
     // 最初何もないことを確認
     expect(manager.stockArr.length).toBe(0);
 
     // 1つ追加した際の確認
-    manager.add(purchasing);
+    manager.add(purchasing, productArr);
     expect(manager.stockArr.length).toBe(1);
     expect(manager.stockArr[0].product.name).toBe('桃');
     expect(manager.stockArr[0].purchaseDate).toEqual(date);
@@ -29,12 +30,71 @@ describe('StockManagerTest', () => {
     // 2つ目追加した際の確認
     date = new Date('2022-07-20');
     purchasing = new Purchasing(new Product('お茶'), new Date('2022-07-20'), 60, 150, 600);
-    manager.add(purchasing);
+    productArr.push(new Product('お茶'));
+
+    manager.add(purchasing, productArr);
     expect(manager.stockArr.length).toBe(2);
     expect(manager.stockArr[1].product.name).toBe('お茶');
     expect(manager.stockArr[1].stock).toBe(600);
     expect(manager.stockArr[1].purchasePrice).toBe(60);
     expect(manager.stockArr[1].sellingPrice).toBe(150);
     expect(manager.stockArr[1].purchaseDate).toEqual(date);
+
+    // ３つ目追加した際の確認(１つ目と同じ名前、１つ目よりも仕入日が古い商品を追加)
+    date = new Date('2022-07-18');
+    purchasing = new Purchasing(new Product('桃'), new Date('2022-07-18'), 300, 500, 300);
+    manager.add(purchasing, productArr);
+    expect(manager.stockArr.length).toBe(3);
+    expect(manager.stockArr[0].product.name).toBe('桃');
+    expect(manager.stockArr[0].purchaseDate).toEqual(date);
+    expect(manager.stockArr[0].purchasePrice).toBe(300);
+    expect(manager.stockArr[0].sellingPrice).toBe(500);
+    expect(manager.stockArr[0].stock).toBe(300);
+  });
+
+  test('reduceStockTest', () => {
+    let manager = new StockManager();
+    let date: Date = new Date('2022-07-20');
+    let purchasing: Purchasing = new Purchasing(new Product('桃'), new Date('2022-07-20'), 200, 400, 200);
+    const productArr: Product[] = [new Product('桃')];
+
+    // 2022-07-20入荷の桃の在庫２００個
+    manager.add(purchasing, productArr);
+    expect(manager.stockArr[0].purchaseDate).toEqual(date);
+    expect(manager.stockArr[0].stock).toBe(200);
+
+    // 2022-07-21入荷の桃の在庫3００個
+    date = new Date('2022-07-21');
+    purchasing = new Purchasing(new Product('桃'), new Date('2022-07-21'), 300, 500, 300);
+    manager.add(purchasing, productArr);
+    expect(manager.stockArr[1].purchaseDate).toEqual(date);
+    expect(manager.stockArr[1].stock).toBe(300);
+
+    // 桃の在庫を300個減らす(仕入日古いほうから)
+    manager.reduceStock('桃', 300);
+    expect(manager.stockArr[0].stock).toBe(0);
+    expect(manager.stockArr[1].stock).toBe(200);
+  });
+
+  test('reduceStockErrorTest', () => {
+    let manager = new StockManager();
+    let date: Date = new Date('2022-07-20');
+    let purchasing: Purchasing = new Purchasing(new Product('桃'), new Date('2022-07-20'), 200, 400, 200);
+    const productArr: Product[] = [new Product('桃')];
+
+    // 2022-07-20入荷の桃の在庫２００個
+    manager.add(purchasing, productArr);
+    expect(manager.stockArr[0].purchaseDate).toEqual(date);
+    expect(manager.stockArr[0].stock).toBe(200);
+
+    // 2022-07-21入荷の桃の在庫3００個
+    date = new Date('2022-07-21');
+    purchasing = new Purchasing(new Product('桃'), new Date('2022-07-21'), 300, 500, 300);
+    manager.add(purchasing, productArr);
+    expect(manager.stockArr[1].purchaseDate).toEqual(date);
+    expect(manager.stockArr[1].stock).toBe(300);
+
+    // 桃の在庫を700個減らす(在庫が不足している)
+    expect(() => manager.reduceStock('桃', 700)).toThrowError('The value is abnormal');
   });
 });
